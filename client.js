1a05f942e1d0ddab03d3f8b3e12da97313fe242e
Vishal Shah <vishal@Vishbook.local>
2026-01-23 11:47:43 -0500

    client testing

--- client.js
+++ client.js
@@ -7,7 +7,7 @@ const alarmSwitch = new Gpio(17, 'in', 'both', { debounceTimeout: 50 });
 
 // This is the URL to the Kasa server endpoint
 const SERVER_URL = 'http://localhost:3000';
-const ENDPOINT = '/toggle-light-bright';
+const ENDPOINT = '/toggle-light-bright'; // this will toggle to full brightness every time
 
 console.log('Monitoring fire alarm switch...');
 
@@ -16,17 +16,19 @@ alarmSwitch.watch(async (err, value) => {
         console.error('GPIO Error:', err);
         return;
     }
+    
+    console.log(value)
 
     // In a Pull-Up configuration, 0 means the switch is pressed (connected to GND)
     if (value === 0) {
         console.log('Switch pressed! Sending signal to Kasa server...');
         
-        try {
-            const response = await axios.post(SERVER_URL + ENDPOINT);
-            console.log('Server response:', response.data.message);
-        } catch (error) {
-            console.error('Could not reach Kasa server:', error.message);
-        }
+        // try {
+        //     const response = await axios.post(SERVER_URL + ENDPOINT);
+        //     console.log('Server response:', response.data.message);
+        // } catch (error) {
+        //     console.error('Could not reach Kasa server:', error.message);
+        // }
     }
 });
 
